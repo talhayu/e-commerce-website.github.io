c9c26750e35805556008fd66ba54266fb6d41605
talhayu <mnhetz999@gmail.com>
2023-02-06 13:44:17 +0500

    header and hero section done

---
+++ index.js
@@ -0,0 +1,32 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    });
+
+    formBtn.addEventListener('click', () =>{
+        loginForm .classList.add('active');
+        });
+    
+    formClose.addEventListener('click', () =>{
+            loginForm .classList.remove('active');
+            });
+
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+  }
+
+  searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+    });
+    
+  
