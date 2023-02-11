051d80bcd3dd144ae108ec7d913e4eab0e3af713
talhayu <mnhetz999@gmail.com>
2023-02-11 13:29:05 +0500

    shop page connected

--- index.js
+++ index.js
@@ -29,4 +29,18 @@ window.onscroll = () =>{
     searchBar.classList.toggle('active');
     });
     
+    const MainImg=document.getElementById("MainImg");
+    const smallimg=document.getElementsByClassName("small-img");
+smallimg[0].onclick = function(){
+MainImg.src=smallimg[0].src
+}
+smallimg[1].onclick = function(){
+MainImg.src=smallimg[1].src
+}
+smallimg[2].onclick = function(){
+MainImg.src=smallimg[2].src
+}
+smallimg[3].onclick = function(){
+MainImg.src=smallimg[3].src
+}
   
